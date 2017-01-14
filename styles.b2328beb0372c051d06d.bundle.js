55a9d5af9218c8ef53e2b0ccbf4bd464b3ba4d99
Rishi Raj <rishiraj824@gmail.com>
2017-01-14 14:20:06 +0530

    query linked with lists

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1069:function(n,o,c){n.exports=c(482)},482:function(n,o){}},[1069]);
+webpackJsonp([1,2],{1049:function(n,o,c){n.exports=c(465)},465:function(n,o){}},[1049]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
