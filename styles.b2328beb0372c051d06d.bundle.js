a56327e26a26fda4ec66c47a8cac0b23ed9d1e34
Rishi Raj <rishiraj824@gmail.com>
2017-01-27 18:28:52 +0530

    deploying the site

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1049:function(n,o,c){n.exports=c(465)},465:function(n,o){}},[1049]);
+webpackJsonp([1,2],{1055:function(n,o,c){n.exports=c(467)},467:function(n,o){}},[1055]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
